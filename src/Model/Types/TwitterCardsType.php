54aa6dae5b4e7ab31aabb4d2958935bf0f6ea04b
jeremy <jeremy@novaway.fr>
2016-02-03 08:50:28 +0100

    Add Twitter Cards types definition

---
+++ src/Model/Types/TwitterCardsType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Novaway\Component\OpenGraph\Model\Types;
+
+class TwitterCardsType
+{
+    const NAMESPACE_TAG = 'twitter';
+
+    const TYPE_APP           = 'app';
+    const TYPE_PLAYER        = 'player';
+    const TYPE_SUMMARY       = 'summary';
+    const TYPE_SUMMARY_LARGE = 'summary_large_image';
+
+    const CARD                      = 'card';
+    const CREATOR                   = 'creator';
+    const CREATOR_ID                = 'creator:id';
+    const DESCRIPTION               = 'description';
+    const IMAGE                     = 'image';
+    const PLAYER                    = 'player';
+    const PLAYER_HEIGHT             = 'player:height';
+    const PLAYER_STREAM             = 'player:stream';
+    const PLAYER_STREAM_CONTENTTYPE = 'player:stream:content_type';
+    const PLAYER_WIDTH              = 'player:width';
+    const SITE                      = 'site';
+    const SITE_ID                   = 'site:id';
+    const TITLE                     = 'title';
+    const URL                       = 'url';
+
+    const APP_COUNTRY         = 'app:country';
+    const APP_IPHONE_ID       = 'app:id:iphone';
+    const APP_IPHONE_NAME     = 'app:name:iphone';
+    const APP_IPHONE_URL      = 'app:url:iphone';
+    const APP_IPAD_ID         = 'app:id:ipad';
+    const APP_IPAD_NAME       = 'app:name:ipad';
+    const APP_IPAD_URL        = 'app:url:ipad';
+    const APP_GOOGLEPLAY_ID   = 'app:id:googleplay';
+    const APP_GOOGLEPLAY_NAME = 'app:name:googleplay';
+    const APP_GOOGLEPLAY_URL  = 'app:url:googleplay';
+}
